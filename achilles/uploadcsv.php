f9660c8e756a719495d5e3fdd59fed937bf02b87
Ret Samys <retsamys@gmx.net>
2019-03-08 11:08:10 +0100

    Create uploadcsv.php

---
+++ achilles/uploadcsv.php
@@ -0,0 +1,31 @@
+<?php
+$FileName=basename($_FILES["fileToUpload"]["name"]);
+$target_file = $_SERVER["DOCUMENT_ROOT"]."/protected/users.csv";
+$uploadOk = 1;
+$FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+
+$feedURL=$_SERVER["DOCUMENT_ROOT"]."/protected/feed.rss";
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($FileType != "csv") {
+    echo "Sorry, only CSV files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+?>
